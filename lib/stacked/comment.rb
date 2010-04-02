db015ab9ff4e65d96e37ce22814fd743882d9ef6
Ryan Bigg <ryan@getup.org.au>
2010-04-03 10:32:36 +1100

    Added two fields that are potentially in comments.

--- lib/stacked/comment.rb
+++ lib/stacked/comment.rb
@@ -3,10 +3,12 @@ module Stacked
     attr_accessor :body,
                   :comment_id,
                   :creation_date,
+                  :edit_count,
                   :owner_display_name,
                   :owner_user_id,
                   :post_id,
                   :post_type,
+                  :reply_to_user_id,
                   :score
 
     alias_method :created_at, :creation_date
