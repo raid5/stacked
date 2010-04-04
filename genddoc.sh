7c36dd016cde8e186ec8dd0fb13aa0f58ee67f5f
Ryan Bigg <ryan@getup.org.au>
2010-04-04 18:45:20 +1000

    Remove all files before trying to copy them back.

--- genddoc.sh
+++ genddoc.sh
@@ -3,6 +3,7 @@ yardoc lib/**/*.rb &&
 mkdir -p ../stacked-doc &&
 mv doc/* ../stacked-doc &&
 git checkout gh-pages &&
+rm -rf * &&
 mv ../stacked-doc/* . &&
 git add . &&
 git commit -m "Updated documentation." &&
