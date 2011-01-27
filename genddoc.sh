e3ce89a143616c4909188f6d93ae315e2ce733bd
Adam McDonald <mcdonald.adam@gmail.com>
2011-01-27 11:07:32 -0800

    minor gendoc update

--- genddoc.sh
+++ genddoc.sh
@@ -4,8 +4,11 @@ rm -rf ../stacked-doc &&
 mkdir -p ../stacked-doc &&
 mv doc/* ../stacked-doc &&
 git checkout gh-pages &&
-rm -rf * &&
+cd .. &&
+rm -rf stacked/* &&
+cd stacked/ &&
 mv ../stacked-doc/* . &&
+rm -rf ../stacked-doc &&
 git add . &&
 git commit -m "Updated documentation." &&
 git push origin gh-pages &&
